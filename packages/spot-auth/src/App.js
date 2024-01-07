bdfb67427976a2e0800641ff8edbc71c7770a4ed
fmruiz <x.francoruiz@gmail.com>
2024-01-07 19:02:49 -0300

    feat(spot-auth): add generate random classname

--- packages/spot-auth/src/App.js
+++ packages/spot-auth/src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import {
+    StylesProvider,
+    createGenerateClassName,
+} from '@material-ui/core/styles';
 
 import SignIn from './components/SignIn';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'au',
+});
 
 export default () => {
     return (
         <div>
-            <BrowserRouter>
-                <Switch>
-                    <Route path={'/'} component={SignIn} />
-                </Switch>
-            </BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path={'/'} component={SignIn} />
+                    </Switch>
+                </BrowserRouter>
+            </StylesProvider>
         </div>
     );
 };
