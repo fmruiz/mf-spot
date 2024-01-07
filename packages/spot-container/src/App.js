4c8255631e6d2aea2ae50d7c440f86f9b7a8cfd4
fmruiz <x.francoruiz@gmail.com>
2024-01-07 19:04:38 -0300

    feat(spot-container): add generate random classname

--- packages/spot-container/src/App.js
+++ packages/spot-container/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import {
+    StylesProvider,
+    createGenerateClassName,
+} from '@material-ui/core/styles';
+
 import AuthApp from './components/AuthApp';
 import Header from './components/Header/Header';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+});
+
 export default () => {
     return (
-        <BrowserRouter>
-            <div>
-                <Header />
-                <AuthApp />
-            </div>
-        </BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <AuthApp />
+                </div>
+            </BrowserRouter>
+        </StylesProvider>
     );
 };
